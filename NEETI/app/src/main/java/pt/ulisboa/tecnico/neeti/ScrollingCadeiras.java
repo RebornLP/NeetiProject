e21082e9d4d167a4c2e7befc47c8df0802f4a0b7
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2016-05-08 20:14:26 +0100

    first try cadeiras

--- NEETI/app/src/main/java/pt/ulisboa/tecnico/neeti/ScrollingCadeiras.java
+++ NEETI/app/src/main/java/pt/ulisboa/tecnico/neeti/ScrollingCadeiras.java
@@ -1,11 +1,51 @@
 package pt.ulisboa.tecnico.neeti;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class ScrollingCadeiras extends ScrollingActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling_cadeiras);
+
+        mainListView = (ListView) findViewById( R.id.listview_cadeiras );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
+
+
+
     }
 }
